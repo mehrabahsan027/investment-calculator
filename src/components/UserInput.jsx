5dd927a3c35abfc10e0f5b265d2698460533569a
mehrabahsan027 <mehrabahsan027@gmail.com>
2024-09-23 21:38:55 +0600

    slight css change for responsive

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 
 function UserInput({ onChangeInput, userInput }) {
   return (
-    <section className="w-full font-poppins h-auto sm:w-8/12 md:text-lg mx-auto  p-10 flex justify-center items-center bg-gradient-to-bl from-emerald-200 to-emerald-500">
-      <div className="flex flex-col gap-10 ">
-        <div className="flex gap-x-10 w-full">
-          <p className="flex flex-col ">
+    <section className="w-full font-poppins h-auto sm:w-8/12 md:text-lg mx-auto  p-5 flex justify-center items-center bg-gradient-to-bl from-emerald-200 to-emerald-500">
+      <div className="flex flex-col gap-y-5 w-full">
+        <div className="flex justify-around  w-full">
+          <p className="flex  flex-col ">
             <label>Initial Investment:</label>
             <input
-            required
+              required
               value={userInput.initialInvestment}
               onChange={(e) =>
                 onChangeInput("initialInvestment", e.target.value)
@@ -20,7 +20,7 @@ function UserInput({ onChangeInput, userInput }) {
           <p className="flex flex-col ">
             <label>Annual Investment:</label>
             <input
-            required
+              required
               value={userInput.annualInvestment}
               onChange={(e) =>
                 onChangeInput("annualInvestment", e.target.value)
@@ -30,21 +30,21 @@ function UserInput({ onChangeInput, userInput }) {
             />
           </p>
         </div>
-        <div className="flex  gap-x-10 w-full">
+        <div className="flex justify-around  w-full">
           <p className="flex flex-col ">
             <label>Expected Return(%):</label>
             <input
-            required
+              required
               value={userInput.expectedReturn}
               onChange={(e) => onChangeInput("expectedReturn", e.target.value)}
               type="number"
               className="border w-32 p-1 rounded-md"
             />
           </p>
-          <p className="flex flex-col ">
+          <p className="flex  flex-col ">
             <label>Duration:</label>
             <input
-            required
+              required
               value={userInput.duration}
               onChange={(e) => onChangeInput("duration", e.target.value)}
               type="number"
